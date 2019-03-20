3a13c0d393182e2aeaaf9c9e6631575b6eacb2f8
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-03-20 11:16:24 -0300

    Adding functions for addintion and substraction.

---
+++ calculator.py
@@ -0,0 +1,10 @@
+"""
+A simple calculator library containing basic math operations.
+"""
+
+def add(first_value, second_value):
+    return first_value + second_value
+
+def sub(first_value, second_value):
+    return first_value + second_value
+
